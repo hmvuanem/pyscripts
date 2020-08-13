e575acebf4c4cf5e90e7a2c942b7a1b6bcbc179a
Hieu Mai <hieu.mai@vuanem.com>
2020-08-13 11:52:40 +0700

    Added C2 Funnel to Pipeline

--- 2gbq.py
+++ 2gbq.py
@@ -19,14 +19,15 @@ from datetime import datetime
 import logging
 import requests
 import time
+from sua_dau_so import change_phone
 
 #create GCP Client
-client = bigquery.Client.from_service_account_json('F:/voltaic-country-280607-ea3eb5348029.json')
-
-headers = {
-    'authorization': 'Bearer VBxdGeTLbdcPs1M',
-    'content-type': 'application/json'
-}
+client = bigquery.Client.from_service_account_json('voltaic-country-280607-ea3eb5348029.json')
+C2M_query = 'https://docs.google.com/spreadsheets/d/13OpfBdAgoiZRERF4SW9zQlopv-_Io3eiLHKylf_y08o/export?format=csv&id=13OpfBdAgoiZRERF4SW9zQlopv-_Io3eiLHKylf_y08o&gid=0'
+C2L_FB_Ads_query = 'https://docs.google.com/spreadsheets/d/1NMAr8S_F92KCN5gl1LJ9I11sIYAyzbzfr2b7oRK7Q_o/export?format=csv&id=1NMAr8S_F92KCN5gl1LJ9I11sIYAyzbzfr2b7oRK7Q_o&gid=734520631'
+C2L_FB_3rd1_query = 'https://docs.google.com/spreadsheets/d/1NMAr8S_F92KCN5gl1LJ9I11sIYAyzbzfr2b7oRK7Q_o/export?format=csv&id=1NMAr8S_F92KCN5gl1LJ9I11sIYAyzbzfr2b7oRK7Q_o&gid=0'
+C2L_FB_3rd2_query = 'https://docs.google.com/spreadsheets/d/1NMAr8S_F92KCN5gl1LJ9I11sIYAyzbzfr2b7oRK7Q_o/export?format=csv&id=1NMAr8S_F92KCN5gl1LJ9I11sIYAyzbzfr2b7oRK7Q_o&gid=1739949882'
+C2L_Shopify_Form_query = 'https://docs.google.com/spreadsheets/d/1DYkPloEJLyAcvLacsW8EnjMHt153-gSdwqxBtEwefOo/export?format=csv&id=1DYkPloEJLyAcvLacsW8EnjMHt153-gSdwqxBtEwefOo&gid=1388039008'
 
 logs = []
 
@@ -130,16 +131,14 @@ sql_specs = {
     'db_name': 'vuanem_ecommerce'
 }
 
-def get_caresoft_tickets(query):
+def get_mysql(query):
     sqlEngine = create_engine(r'mysql+pymysql://{0}:{1}@{2}/{3}'.format(sql_specs['username'], sql_specs['pwd'], sql_specs['server'], sql_specs['db_name']))
     dbConnection = sqlEngine.connect()
-    caresoft_tickets = pd.read_sql(query, dbConnection)
-    print(caresoft_tickets.head())
-    return caresoft_tickets
+    df = pd.read_sql(query, dbConnection)
+    print(df.head())
+    return df
 
-caresoft_tickets = get_caresoft_tickets(caresoft_tickets_query)
-
-caresoft_tickets.to_excel('caresoft_tickets.xlsx', index=False)
+caresoft_tickets = get_mysql(caresoft_tickets_query)
 
 caresoft_tickets_schema = [
     bigquery.SchemaField('care_soft_ticket_id', "INTEGER"),
@@ -164,23 +163,17 @@ load_gbq(
     schema=caresoft_tickets_schema
 )
 
-caresoft_tickets_custom_fields_query = '''
-SELECT
-	ticket_id,
-	SUM(CASE WHEN custom_filed_id = 4871 THEN custom_filed_value ELSE null END) AS trang_thai_cham_soc,
-	SUM(CASE WHEN custom_filed_id = 4893 THEN custom_filed_value ELSE null END) AS trang_thai_tiep_nhan
-FROM
-	vuanem_ecommerce.care_soft_ticket_custom_fileds cstcf
-GROUP BY ticket_id
-'''
-
-caresoft_tickets_custom_fields = get_caresoft_tickets(caresoft_tickets_custom_fields_query)
-
-r2 = requests.get('https://api.caresoft.vn/VUANEM/api/v1/tickets/custom_fields', headers=headers).json()
-care_status_df = pd.DataFrame(r2['custom_fields'][0])
-care_status_df = pd.json_normalize(care_status_df.to_dict('list'), ['values']).apply(pd.Series)
-receive_status_df = pd.DataFrame(r2['custom_fields'][1])
-receive_status_df = pd.json_normalize(receive_status_df.to_dict('list'), ['values']).apply(pd.Series)
+def get_care_receive_status():
+    headers = {
+        'authorization': 'Bearer VBxdGeTLbdcPs1M',
+        'content-type': 'application/json'
+    }
+    r2 = requests.get('https://api.caresoft.vn/VUANEM/api/v1/tickets/custom_fields', headers=headers).json()
+    care_status_df = pd.DataFrame(r2['custom_fields'][0])
+    care_status_df = pd.json_normalize(care_status_df.to_dict('list'), ['values']).apply(pd.Series)
+    receive_status_df = pd.DataFrame(r2['custom_fields'][1])
+    receive_status_df = pd.json_normalize(receive_status_df.to_dict('list'), ['values']).apply(pd.Series)
+    return care_status_df, receive_status_df
 
 def transform_tickets_custom_fields(df):
     df = df.merge(care_status_df, how='left', left_on='trang_thai_cham_soc', right_on='id')
@@ -191,6 +184,20 @@ def transform_tickets_custom_fields(df):
         'lable_y': 'trang_thai_tiep_nhan'
     }, axis=1, inplace=True)
     return df
+
+caresoft_tickets_custom_fields_query = '''
+SELECT
+	ticket_id,
+	SUM(CASE WHEN custom_filed_id = 4871 THEN custom_filed_value ELSE null END) AS trang_thai_cham_soc,
+	SUM(CASE WHEN custom_filed_id = 4893 THEN custom_filed_value ELSE null END) AS trang_thai_tiep_nhan
+FROM
+	vuanem_ecommerce.care_soft_ticket_custom_fileds cstcf
+GROUP BY ticket_id
+'''
+
+caresoft_tickets_custom_fields = get_mysql(caresoft_tickets_custom_fields_query)
+
+care_status_df, receive_status_df = get_care_receive_status()
 
 caresoft_tickets_custom_fields = transform_tickets_custom_fields(caresoft_tickets_custom_fields)
 
@@ -225,7 +232,7 @@ FROM
     vuanem_ecommerce.care_soft_call_logs cscl
 '''
 
-caresoft_call_logs = get_caresoft_tickets(caresoft_call_logs_query)
+caresoft_call_logs = get_mysql(caresoft_call_logs_query)
 
 caresoft_call_logs_schema = [
     bigquery.SchemaField('call_id', "STRING"),
@@ -247,6 +254,116 @@ load_gbq(
     schema=caresoft_call_logs_schema
 )
 
+# FUNNEL
+
+vuanem_salescall_salescall_query = '''
+SELECT
+    customer_name AS name,
+    customer_tel AS phone,
+    customer_email AS email,
+    gclid,
+    campain,
+    from_landing,
+    created_at AS dt,
+    CAST(created_at AS date) as date,
+    shopify_order_id,
+    (CASE WHEN shopify_order_id <> '0' THEN 'Shopify Order' ELSE 'LP EC' END) AS source
+FROM
+    vuanem_ecommerce.vuanem_salescall_salescall vss
+'''
+
+vuanem_call = '''
+SELECT
+    call_date AS dt,
+    CAST(call_date AS date) AS date,
+    phone,
+    status,
+    'Hotline' AS channel
+FROM
+    vuanem_ecommerce.vuanem_call
+WHERE hotline = '18002092'
+ORDER BY call_date
+'''
+
+C2C = get_mysql(vuanem_call)
+
+C2M = pd.read_csv(
+    C2M_query,
+    usecols=['Tên', 'Phone(raw)', 'Date(raw)'],
+    dtype={'Tên':str, 'Phone(raw)':str, 'Date(raw)':str})
+C2M['channel'] = 'Messages'
+C2M.rename(mapper={
+    'Tên': 'name',
+    'Phone(raw)': 'phone',
+    'Date(raw)': 'date'
+},axis=1,inplace=True)
+C2M['dt'] = pd.to_datetime(C2M['date'], format='%d/%m/%Y')
+C2M['date'] = pd.to_datetime(C2M['date'], format='%d/%m/%Y').dt.date
+
+C2L_vuanem_salescalls = get_mysql(vuanem_salescall_salescall_query)
+C2L_vuanem_salescalls['shopify_order_id'] = C2L_vuanem_salescalls['shopify_order_id'].astype(str)
+
+C2L_FB_Ads = pd.read_csv(C2L_FB_Ads_query, usecols=['Date', 'Name', 'Phone', 'Email'], parse_dates=['Date'])
+C2L_FB_Ads['source'] = 'LP Digital - FB Ads'
+C2L_FB_Ads.columns = C2L_FB_Ads.columns.str.lower()
+C2L_FB_Ads.rename(mapper={
+    'date': 'dt'
+},axis=1,inplace=True)
+C2L_FB_Ads['date'] = C2L_FB_Ads['dt'].dt.date
+
+C2L_FB_3rd1 = pd.read_csv(C2L_FB_3rd1_query, usecols=['Date', 'Name', 'Phone', 'Email', 'Gclid'], parse_dates=['Date'])
+C2L_FB_3rd1.columns = C2L_FB_3rd1.columns.str.lower()
+C2L_FB_3rd1['source'] = 'LP Digital - FB Ads 3rd1'
+C2L_FB_3rd1.rename(mapper={
+    'date': 'dt'
+},axis=1,inplace=True)
+C2L_FB_3rd1['date'] = C2L_FB_3rd1['dt'].dt.date
+
+C2L_FB_3rd2 = pd.read_csv(C2L_FB_3rd2_query, usecols=['Date', 'Name', 'Phone', 'Email', 'Gclid'], parse_dates=['Date'])
+C2L_FB_3rd2.columns = C2L_FB_3rd2.columns.str.lower()
+C2L_FB_3rd2['source'] = 'LP Digital - FB Ads'
+C2L_FB_3rd2.rename(mapper={
+    'date': 'dt'
+},axis=1,inplace=True)
+C2L_FB_3rd2['date'] = C2L_FB_3rd2['dt'].dt.date
+
+C2L_Shopify_Form = pd.read_csv(C2L_Shopify_Form_query, usecols=['First Subscribed On', 'Phone Number', 'Email', 'gclid', 'initial_utm_medium', 'initial_utm_source'], parse_dates=['First Subscribed On'],date_parser=lambda col: pd.to_datetime(col, utc=True))
+C2L_Shopify_Form.rename(mapper={
+    'First Subscribed On': 'dt',
+    'Phone Number': 'phone',
+    'Email': 'email'
+}, axis=1, inplace=True)
+C2L_Shopify_Form['source'] = 'LP Digital - Shopify Form'
+C2L_Shopify_Form['dt']= C2L_Shopify_Form['dt'].dt.tz_localize(None)
+C2L_Shopify_Form['date'] = C2L_Shopify_Form['dt'].dt.date
+
+C2L = pd.concat([C2L_FB_Ads, C2L_FB_3rd1, C2L_FB_3rd2, C2L_Shopify_Form, C2L_vuanem_salescalls])
+C2L['channel'] = 'Leads'
+
+C2Leads = pd.concat([C2C, C2M, C2L])
+C2Leads['phone'] = C2Leads['phone'].apply(change_phone)
+
+C2Leads_schema = [
+    bigquery.SchemaField('dt', "TIMESTAMP"),
+    bigquery.SchemaField('date', "DATE"),
+    bigquery.SchemaField("phone", "STRING"),
+    bigquery.SchemaField('status', "STRING"),
+    bigquery.SchemaField("channel", "STRING"),
+    bigquery.SchemaField('name', "STRING"),
+    bigquery.SchemaField('email', "STRING"),
+    bigquery.SchemaField('source', "STRING"),
+    bigquery.SchemaField('gclid', "STRING"),
+    bigquery.SchemaField('initial_utm_medium', "STRING"),
+    bigquery.SchemaField('initial_utm_source', "STRING"),
+    bigquery.SchemaField('campain', "STRING"),
+    bigquery.SchemaField('from_landing', "STRING"),
+    bigquery.SchemaField('shopify_order_id', "STRING")]
+
+load_gbq(
+    df=C2Leads,
+    table_id='C2Leads.C2Leads',
+    schema=C2Leads_schema
+)
 client.close()
 
 logging.basicConfig(filename='2GBQ.log', level=logging.INFO)

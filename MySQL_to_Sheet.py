30909fce849e77603f0ba7818c9f223585880fda
Hieu Mai <hieu.mai@vuanem.com>
2020-08-19 11:52:38 +0700

    Generalized MySQL to Sheet

--- MySQL_to_Sheet.py
+++ MySQL_to_Sheet.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
-
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
@@ -14,29 +8,19 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+def get_mysql(query, db):
+    username = 'root'
+    pwd = 'VuaNem@2020'
+    server = '1.55.215.47'
+    db_name = db
+    sqlEngine = create_engine(r'mysql+pymysql://{0}:{1}@{2}/{3}'.format(username, pwd, server, db_name))
+    dbConnection = sqlEngine.connect()
+    df = pd.read_sql(query, dbConnection)
+    return df
 
-# In[19]:
-
-
-username = 'root'
-pwd = 'VuaNem@2020'
-server = '1.55.215.47'
-db_name = 'vuanem_landing'
-
-
-# In[20]:
-
-
-sqlEngine = create_engine(r'mysql+pymysql://{0}:{1}@{2}/{3}'.format(username, pwd, server, db_name))
-dbConnection = sqlEngine.connect()
-
-
-# In[21]:
-
-
-query = '''
+C2L_query = '''
 SELECT
-    ID,
+    salescall_id,
     customer_name,
     customer_tel,
     customer_email,
@@ -44,30 +28,30 @@ SELECT
     campain,
     created_at
 FROM
-    km_salescall_index
+    vuanem_salescall_salescall
 '''
 
+LuckyWheel_query = '''
+SELECT
+   	km_posts.ID AS ID,
+	km_posts.post_name AS telephone,
+	km_postmeta.meta_value AS data,
+	km_posts.post_date
+FROM km_posts 
+left join km_postmeta on km_postmeta.post_id = km_posts.ID
+WHERE km_posts.post_type = 'wplwl_email'
+'''
 
-# In[22]:
+c2l_df = get_mysql(C2L_query, 'vuanem_ecommerce')
+c2l_df['created_at'] = c2l_df['created_at'].dt.date.astype(str)
+c2l_df = c2l_df.drop_duplicates(['customer_name', 'customer_tel', 'gclid', 'campain', 'created_at'])
 
-
-df = pd.read_sql(query, dbConnection)
-
-
-# In[23]:
-
-
-df['created_at'] = df['created_at'].dt.date.astype(str)
-df = df.drop_duplicates(['customer_name', 'customer_tel', 'gclid', 'campain', 'created_at'])
-
-# In[24]:
-
-
+LuckyWheel_df = get_mysql(LuckyWheel_query, 'vuanem_landing')
+LuckyWheel_df['post_date'] = pd.to_datetime(LuckyWheel_df['post_date']).dt.date.astype(str)
+print(LuckyWheel_df.dtypes)
 SAMPLE_SPREADSHEET_ID_input = '1DYkPloEJLyAcvLacsW8EnjMHt153-gSdwqxBtEwefOo'
-non_luckywheel = r"'C2L'!A1:G1000000"
-
-# In[25]:
-
+c2l = r"'C2L'!A1:G1000000"
+LuckyWheel = r"'LuckyWheel'!A1:D1000000"
 
 def Create_Service(client_secret_file, api_service_name, api_version, *scopes):
     global service
@@ -94,15 +78,7 @@ def Create_Service(client_secret_file, api_service_name, api_version, *scopes):
     except Exception as e:
         print(e)
 
-
-# In[26]:
-
-
 Create_Service(r'F:/credentials.json', 'sheets', 'v4',['https://www.googleapis.com/auth/spreadsheets'])
-
-
-# In[27]:
-
 
 def Export_Data_To_Sheets(table, range_name):
     response_date = service.spreadsheets().values().update(
@@ -115,5 +91,7 @@ def Export_Data_To_Sheets(table, range_name):
     ).execute()
     print('Done' + range_name)
 
-Export_Data_To_Sheets(df, non_luckywheel)
+Export_Data_To_Sheets(c2l_df, c2l)
+Export_Data_To_Sheets(LuckyWheel_df, LuckyWheel)
+
 print('Done at 1DYkPloEJLyAcvLacsW8EnjMHt153-gSdwqxBtEwefOo')
